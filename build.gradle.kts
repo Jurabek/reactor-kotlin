8b5657db92210dbd4aa1565615cfd93bcc0d4ab2
jurabek <jurabek.azizkhujaev@gmail.com>
2020-02-27 12:13:58 +0100

    missed gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.4.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	kotlin("jvm") version "1.3.61"
+	kotlin("plugin.spring") version "1.3.61"
+}
+
+group = "com.jurabek"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("io.github.microutils:kotlin-logging:1.7.6")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+	testImplementation("com.squareup.okhttp3:mockwebserver:3.10.0")
+	testImplementation("io.projectreactor:reactor-test")
+
+	testImplementation("org.junit.jupiter:junit-jupiter-params:5.5.2")
+	testImplementation( "org.junit.jupiter:junit-jupiter-api:5.5.2")
+	testImplementation("org.mockito:mockito-core:2.+")
+	testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.+")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
